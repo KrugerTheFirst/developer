3c0dae297835f39dbc7257731fee21a415ab9a03
kaniaTaxi <serwisy@agresso.pl>
2017-05-21 23:07:35 +0200

    feat(form): add breakpoints and images

---
+++ reservations.js
@@ -0,0 +1,14 @@
+var Reservations = {
+    'place_1': {
+        'disabled': false,
+        'houses' : []
+    },
+    'place_2': {
+        'disabled': false,
+        'houses' : []
+    },
+    'place_3': {
+        'disabled': false,
+        'houses' : []
+    },
+};
--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,51 @@
+var Houses = {
+    '1a': {
+        'name': 'Dom 1A',
+        'sibling': '1b'
+    },
+    '1b': {
+        'name': 'Dom 1B',
+        'sibling': '1a'
+    },
+    '2a': {
+        'name': 'Dom 2A',
+        'sibling': '2b'
+    },
+    '2b': {
+        'name': 'Dom 2B',
+        'sibling': '2a'
+    },
+    '3': {
+        'name': 'Dom 3'
+    }
+};
+
+
 var Dropdown = {
     $container: null,
+    $houses: null,
+    $places: null,
 
     init: function () {
         var _ = this;
 
         _.$container = $('[data-dropdown]');
+        _.$houses = _.$container.filter('[data-dropdown="houses"]');
+        _.$places = _.$container.filter('[data-dropdown="places"]');
+
         _.$container.find('a').on('click', function () {
             _.toggle($(this).parent());
         });
 
-        _.$container.find('input[type="checkbox"]').on('change', function () {
-            _.checkboxes(this);
+        _.$container.find('input[type="radio"]').on('change', function () {
+            _.radios(this);
         });
+
+        _.disableRadios();
+        _.showHousesWhenYouSelectPlace();
+        _.showPlaceForHouse();
     },
+
 
     toggle: function ($context) {
         if (!$context.hasClass('contact__form_dropdown--disabled')) {
@@ -26,7 +59,7 @@ var Dropdown = {
             .removeClass('contact__form_dropdown--open');
 
         $context.find('li').removeClass('selected');
-        $context.find('input[type="checkbox"]').prop('checked', false);
+        $context.find('input[type="radio"]').prop('checked', false);
     },
 
     enableNext: function (id) {
@@ -34,18 +67,83 @@ var Dropdown = {
         $context.removeClass('contact__form_dropdown--disabled');
     },
 
-    checkboxes: function (context) {
+    radios: function (context) {
         var _ = this,
-            $parent = $(context).closest('[data-dropdown]'),
-            id = parseInt($parent.attr('data-dropdown'));
+            $parent = $(context).closest('[data-dropdown]');
 
-        $(context).closest('li').toggleClass('selected', $(context).is(':checked'));
+        $parent.find('li').removeClass('selected');
+        $(context).closest('li').addClass('selected');
 
-        if ($parent.find('input[type="checkbox"]:checked').length > 0) {
-            _.enableNext(id + 1);
+        if ($parent.find('input[type="radio"]:checked').length > 0) {
+            _.enableNext('places');
         } else {
-            _.disableNext(id + 1);
+            _.disableNext('places');
         }
+    },
+
+    showHousesWhenYouSelectPlace: function () {
+        var _ = this;
+
+        _.$places.find('input[type="radio"]').on('change', function () {
+            var actual = $(this).val();
+
+            if (Reservations[actual] !== undefined) {
+                var reservation = Reservations[actual];
+                if (reservation.houses.length > 0) {
+                    _.$houses.find('li').addClass('disabled');
+                    _.$houses.find('input[type="radio"]').prop('disabled', true);
+
+                    for (var i = 0; i < reservation.houses.length; ++i) {
+                        var house = reservation.houses[i];
+                        if (Houses[house] !== undefined) {
+                            _.$houses.find('li[data-house="' + Houses[house].sibling + '"]').removeClass('disabled');
+                            _.$houses.find('input[value="' + Houses[house].sibling + '"]').prop('disabled', false);
+                        }
+                    }
+                } else {
+                    _.$houses.find('li').removeClass('disabled');
+                    _.$houses.find('input[type="radio"]').prop('disabled', false);
+                
+                }
+            }
+        });
+    },
+
+    showPlaceForHouse: function () {
+        var _ = this;
+        _.$houses.find('input[type="radio"]').on('change', function () {
+            var actual = $(this).val(),
+                places = Object.keys(Reservations);
+
+            for (var i = 0; i < places.length; ++i) {
+                var name = places[i],
+                    reservationPlace = Reservations[name];
+
+                if (reservationPlace.houses.indexOf(Houses[actual].sibling) > -1) {
+                    _.$places.find('input[value="' + name + '"]').prop('checked', true);
+                    _.$places.find('li[data-place="' + name + '"]').addClass('selected');
+
+                    _.$houses.find('li').not('li[data-house="' + actual + '"]').addClass('disabled');
+                    _.$houses.find('input').not('input[value="' + actual + '"]').prop('disabled', true);
+                }
+            }
+        });
+    },
+
+    disableRadios: function () {
+        var _ = this,
+            places = Object.keys(Reservations);
+        for (var i = 0; i < places.length; ++i) {
+            var name = places[i],
+                reservationPlace = Reservations[name];
+
+            if (reservationPlace.disabled) {
+                var $radio = _.$places.find('input[value="' + name + '"]');
+                $radio.prop('disabled', true);
+                $radio.closest('li').addClass('disabled');
+            }
+        }
+
     }
 };
 
