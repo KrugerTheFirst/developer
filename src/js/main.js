64bdc2b68a279cfe8a9c301ff6c3af569a8927a7
kaniaTaxi <serwisy@agresso.pl>
2017-05-24 21:45:28 +0200

    feat(map): add google map

--- src/js/main.js
+++ src/js/main.js
@@ -149,10 +149,26 @@ var Dropdown = {
 
 
 
-
+function map(){
+    var $map = $('[data-map]');
+    if($map.length > 0){
+        $map.googleMap({
+            zoom: 17, // Initial zoom level (optional)
+            coords: [51.0990726,17.0209529], // Map center (optional)
+            type: "ROADMAP", // Map type (optional)
+            styles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}]
+        }).addMarker({
+            coords: [48.895651, 2.290569],
+            icon: 'build/img/pin.png',
+            url: 'http://maps.google.com'
+        });
+    }
+}
 
 $(function () {
     Dropdown.init();
+
+    map();
 
     $('[data-slider]').slick({
         dots: true,
